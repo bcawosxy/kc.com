3bed8251f3f1501ab77dbb97bb7eaba1166b43f2
Mars-Dev <bcawosxy@gmail.com>
2017-11-22 12:49:53 +0800

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel Edit by Vmage
                 </div>
 
                 <div class="links">
